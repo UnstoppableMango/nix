bd2fa06fef10180167f476c3f2e6ea8d0bb4b3c9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-12-19 13:52:51 -0600

    Fix chart-releaser

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
-NIX ?= nix
+NIX       ?= nix
+GOMOD2NIX ?= gomod2nix
 
 check:
 	$(NIX) flake check --all-systems
 
-build:
+build: pkgs/chart-releaser/gomo2nix.toml
 	$(NIX) build
+
+# TODO: The generated toml file isn't correct
+pkgs/chart-releaser/gomo2nix.toml:
+	$(GOMOD2NIX) generate github.com/helm/chart-releaser --outdir ${@D}
 
 .vscode/settings.json: hack/vscode.json
 	mkdir -p ${@D} && cp $< $@
--- pkgs/chart-releaser/default.nix
+++ pkgs/chart-releaser/default.nix
@@ -1,72 +1,64 @@
 {
-  buildGoApplication,
-  coreutils,
-  fetchFromGitHub,
-  git,
-  installShellFiles,
-  kubectl,
-  kubernetes-helm,
-  lib,
-  makeWrapper,
-  yamale,
-  yamllint,
-}:
+  perSystem =
+    { pkgs, lib, ... }:
+    {
+      packages.chart-releaser = pkgs.buildGoApplication rec {
+        pname = "chart-releaser";
+        version = "1.8.1";
 
-buildGoApplication rec {
-  pname = "chart-releaser";
-  version = "1.8.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "helm";
+          repo = "chart-releaser";
+          rev = "v${version}";
+          hash = "sha256-h1czHb/xK+kOEK4TJhMnwnLeVmQm52C8dTUy+fahJ90=";
+        };
 
-  src = fetchFromGitHub {
-    owner = "helm";
-    repo = "chart-releaser";
-    rev = "v${version}";
-    hash = "sha256-h1czHb/xK+kOEK4TJhMnwnLeVmQm52C8dTUy+fahJ90=";
-  };
+        modules = ./gomod2nix.toml;
 
-  # TODO
-  # modules = ./gomod2nix.toml;
+        postPatch = ''
+          substituteInPlace pkg/config/config.go \
+            --replace "\"/etc/cr\"," "\"$out/etc/cr\","
+        '';
 
-  postPatch = ''
-    substituteInPlace pkg/config/config.go \
-      --replace "\"/etc/cr\"," "\"$out/etc/cr\","
-  '';
+        ldflags = [
+          "-w"
+          "-s"
+          "-X github.com/helm/chart-releaser/cr/cmd.Version=${version}"
+          "-X github.com/helm/chart-releaser/cr/cmd.GitCommit=${src.rev}"
+          "-X github.com/helm/chart-releaser/cr/cmd.BuildDate=19700101-00:00:00"
+        ];
 
-  ldflags = [
-    "-w"
-    "-s"
-    "-X github.com/helm/chart-releaser/cr/cmd.Version=${version}"
-    "-X github.com/helm/chart-releaser/cr/cmd.GitCommit=${src.rev}"
-    "-X github.com/helm/chart-releaser/cr/cmd.BuildDate=19700101-00:00:00"
-  ];
+        nativeBuildInputs = with pkgs; [
+          git
+          installShellFiles
+          makeWrapper
+        ];
 
-  nativeBuildInputs = [
-    installShellFiles
-    makeWrapper
-  ];
+        postInstall = ''
+          installShellCompletion --cmd cr \
+            --bash <($out/bin/cr completion bash) \
+            --zsh <($out/bin/cr completion zsh) \
+            --fish <($out/bin/cr completion fish) \
 
-  postInstall = ''
-    installShellCompletion --cmd cr \
-      --bash <($out/bin/cr completion bash) \
-      --zsh <($out/bin/cr completion zsh) \
-      --fish <($out/bin/cr completion fish) \
+          wrapProgram $out/bin/cr --prefix PATH : ${
+            lib.makeBinPath [
+              pkgs.coreutils
+              pkgs.git
+              pkgs.kubectl
+              pkgs.kubernetes-helm
+              pkgs.yamale
+              pkgs.yamllint
+            ]
+          }
+        '';
 
-    wrapProgram $out/bin/cr --prefix PATH : ${
-      lib.makeBinPath [
-        coreutils
-        git
-        kubectl
-        kubernetes-helm
-        yamale
-        yamllint
-      ]
-    }
-  '';
-
-  meta = with lib; {
-    description = "Hosting Helm Charts via GitHub Pages and Releases";
-    homepage = "https://github.com/helm/chart-releaser";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ UnstoppableMango ];
-    mainProgram = "cr";
-  };
+        meta = with lib; {
+          description = "Hosting Helm Charts via GitHub Pages and Releases";
+          homepage = "https://github.com/helm/chart-releaser";
+          license = licenses.asl20;
+          maintainers = with maintainers; [ UnstoppableMango ];
+          mainProgram = "cr";
+        };
+      };
+    };
 }
