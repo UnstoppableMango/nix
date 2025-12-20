8884b5a41cb3f91b011e3192d91d2ef00d4bcebf
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-12-19 18:20:18 -0600

    WIP on the actions-runner

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
       imports = [
         inputs.treefmt-nix.flakeModule
 
+        ./packages/actions-runner
         ./packages/aspire-cli
         ./packages/chart-releaser
         ./packages/mmake
---
+++ packages/actions-runner/default.nix
@@ -0,0 +1,44 @@
+{
+  perSystem =
+    { pkgs, system, ... }:
+    let
+      # Map from NixOS system names to dev.sh layout names
+      rids = {
+        x86_64-linux = "linux-x64";
+        aarch64-linux = "linux-arm64";
+      };
+
+      rid = rids.${system};
+
+      # https://github.com/actions/runner/blob/main/src/global.json#L3
+      dotnet = pkgs.dotnetCorePackages.sdk_8_0;
+    in
+    {
+      packages.actions-runner = pkgs.stdenv.mkDerivation rec {
+        pname = "actions-runner";
+        version = "2.330.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "actions";
+          repo = "runner";
+          tag = "v${version}";
+          hash = "sha256-9/W2QYC9iAGVqvO82SsyBXJp0q1irhJq0VSUXPuDCCA=";
+        };
+
+        buildInputs = with pkgs; [
+          bash
+          curl
+          dotnet
+          git
+          powershell
+          which
+        ];
+
+        buildPhase = ''
+          cd src
+          sed -i '5,7d;30d' ./dir.proj
+          dotnet msbuild -t:Build -p:PackageRuntime="${rid}" -p:BUILDCONFIG="Release" -p:RunnerVersion="${version}" -p:GitInfoCommitHash="TODO" ./dir.proj
+        '';
+      };
+    };
+}
