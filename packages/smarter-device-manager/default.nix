3f5f495fe08b0121c4245d9d328bfa3c5757c491
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-04-23 20:01:30 -0500

    fmt

--- packages/smarter-device-manager/default.nix
+++ packages/smarter-device-manager/default.nix
@@ -4,14 +4,14 @@
     let
       pname = "smarter-device-manager";
       version = "1.20.12";
-      upstreamSrc = pkgs.fetchFromGitHub {
+      src = pkgs.fetchFromGitHub {
         owner = "smarter-project";
         repo = pname;
         rev = "v${version}";
         hash = "sha256-uACRrhlSzGctl+ZeSIM2QLI4Uwr1uFbh+m5qpg06Ahs=";
       };
 
-      updateDeps = import ./update-deps.nix { inherit pkgs; src = upstreamSrc; };
+      updateDeps = import ./update-deps.nix { inherit src pkgs; };
     in
     {
       apps.update-smarter-device-manager-deps = {
@@ -22,23 +22,11 @@
       packages.smarter-device-manager = pkgs.buildGoApplication {
         inherit pname version;
 
-        src = pkgs.runCommand "${pname}-${version}-src" { } ''
-          mkdir $out
-          cp -r ${upstreamSrc}/. $out
-          chmod -R u+w $out
-          cp ${./go.mod} $out/go.mod
-          cp ${./go.sum} $out/go.sum
-        '';
-
         modules = ./gomod2nix.toml;
 
         nativeBuildInputs = with pkgs; [
           makeWrapper
         ];
-
-        postPatch = ''
-          rm -rf vendor
-        '';
 
         postInstall = ''
           install -Dm644 conf.yaml $out/share/${pname}/conf.yaml
