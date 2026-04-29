654003c4a58a34cf1f6a6853e9b410620195efe3
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-04-28 22:41:52 -0500

    WIP

--- builders/upjet/provider-repo.nix
+++ builders/upjet/provider-repo.nix
@@ -16,57 +16,59 @@
   terraformDocsPath ? "docs/resources",
   version,
 }@args:
-stdenv.mkDerivation {
-  # https://github.com/crossplane/upjet/blob/main/docs/generating-a-provider.md
-  inherit pname version;
+stdenv.mkDerivation (
+  {
+    # https://github.com/crossplane/upjet/blob/main/docs/generating-a-provider.md
+    inherit pname version;
 
-  src =
-    if builtins.hasAttr "src" args then
-      builtins.getAttr "src" args
-    else
-      fetchFromGitHub {
-        owner = "crossplane";
-        repo = "upjet-provider-template";
-        rev = "96440083ef6ed070d9413436a9d6a40000d6773f";
-        hash = "sha256-OhXPzgzaXmaWsgFow1wocyMoFY4Apb7Lj552I248l50=";
-        fetchSubmodules = true;
-      };
+    src =
+      if builtins.hasAttr "src" args then
+        builtins.getAttr "src" args
+      else
+        fetchFromGitHub {
+          owner = "crossplane";
+          repo = "upjet-provider-template";
+          rev = "96440083ef6ed070d9413436a9d6a40000d6773f";
+          hash = "sha256-OhXPzgzaXmaWsgFow1wocyMoFY4Apb7Lj552I248l50=";
+          fetchSubmodules = true;
+        };
 
-  patches = [ ./Makefile.patch ];
+    patches = [ ./Makefile.patch ];
 
-  nativeBuildInputs = [ git ];
+    nativeBuildInputs = [ git ];
 
-  PROVIDER_NAME_LOWER = providerNameLower;
-  PROVIDER_NAME_NORMAL = providerName;
-  ORGANIZATION_NAME = organizationName;
-  CRD_ROOT_GROUP = crdRootGroup;
+    PROVIDER_NAME_LOWER = providerNameLower;
+    PROVIDER_NAME_NORMAL = providerName;
+    ORGANIZATION_NAME = organizationName;
+    CRD_ROOT_GROUP = crdRootGroup;
 
-  # This is terrible, but feels less error-prone than translating `git grep` to `grep`
-  preConfigure = ''
-    patchShebangs hack/prepare.sh
-    git init -b main && git add .
-  '';
+    # This is terrible, but feels less error-prone than translating `git grep` to `grep`
+    preConfigure = ''
+      patchShebangs hack/prepare.sh
+      git init -b main && git add .
+    '';
 
-  configurePhase = ''
-    runHook preConfigure
-    hack/prepare.sh
-    runHook postConfigure
-  '';
+    configurePhase = ''
+      runHook preConfigure
+      hack/prepare.sh
+      runHook postConfigure
+    '';
 
-  buildPhase = ''
-    runHook preBuild
-    substituteInPlace Makefile \
-      --subst-var-by 'TERRAFORM_PROVIDER_SOURCE' '${terraformProviderSource}' \
-      --subst-var-by 'TERRAFORM_PROVIDER_REPO' '${terraformProviderRepo}' \
-      --subst-var-by 'TERRAFORM_PROVIDER_VERSION' '${terraformProviderVersion}' \
-      --subst-var-by 'TERRAFORM_PROVIDER_DOWNLOAD_NAME' '${terraformProviderDownloadName}' \
-      --subst-var-by 'TERRAFORM_NATIVE_PROVIDER_BINARY' '${terraformNativeProviderBinary}' \
-      --subst-var-by 'TERRAFORM_DOCS_PATH' '${terraformDocsPath}'
-    runHook postBuild
-  '';
+    buildPhase = ''
+      runHook preBuild
+      substituteInPlace Makefile \
+        --subst-var-by 'TERRAFORM_PROVIDER_SOURCE' '${terraformProviderSource}' \
+        --subst-var-by 'TERRAFORM_PROVIDER_REPO' '${terraformProviderRepo}' \
+        --subst-var-by 'TERRAFORM_PROVIDER_VERSION' '${terraformProviderVersion}' \
+        --subst-var-by 'TERRAFORM_PROVIDER_DOWNLOAD_NAME' '${terraformProviderDownloadName}' \
+        --subst-var-by 'TERRAFORM_NATIVE_PROVIDER_BINARY' '${terraformNativeProviderBinary}' \
+        --subst-var-by 'TERRAFORM_DOCS_PATH' '${terraformDocsPath}'
+      runHook postBuild
+    '';
 
-  installPhase = ''
-    cp -r . $out
-  '';
-}
-// args
+    installPhase = ''
+      cp -r . $out
+    '';
+  }
+  // args
+)
--- builders/upjet/provider.nix
+++ builders/upjet/provider.nix
@@ -1,24 +1,26 @@
 {
   buildGoApplication,
   buildUpjetProviderRepo,
-  externalNames ? {},
+  externalNames ? { },
   pname,
   version,
   ...
 }@args:
-buildGoApplication {
-  inherit pname version;
+buildGoApplication (
+  {
+    inherit pname version;
 
-  src =
-    if builtins.hasAttr "src" args then
-      builtins.getAttr "src" args
-    else
-      import buildUpjetProviderRepo ({ inherit pname version; } // args);
+    src =
+      if builtins.hasAttr "src" args then
+        builtins.getAttr "src" args
+      else
+        import buildUpjetProviderRepo ({ inherit pname version; } // args);
 
-  buildPhase = ''
-    runHook preBuild
-    # cp ${externalNames}
-    runHook postBuild
-  '';
-}
-// args
+    buildPhase = ''
+      runHook preBuild
+      # cp ${externalNames}
+      runHook postBuild
+    '';
+  }
+  // args
+)
