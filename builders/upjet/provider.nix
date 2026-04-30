1cc96570c2db7b372e364c986261b89e42586d8e
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-04-29 23:09:44 -0500

    WIP

--- builders/upjet/provider.nix
+++ builders/upjet/provider.nix
@@ -1,7 +1,10 @@
 {
   buildGoApplication,
   buildUpjetProviderRepo,
+  coreutils,
   externalNames ? { },
+  gnumake,
+  gotools,
   modules,
   pname,
   version,
@@ -20,11 +23,15 @@ buildGoApplication {
 
   inherit (src) passthru;
 
+  nativeBuildInputs = [ coreutils gnumake gotools ];
+
   preBuild = ''
     sed -i '8,10d;36d;60d' config/provider.go
     sed -i '12,15d;21,23d' apis/cluster/zz_register.go
     sed -i '12,15d;21,23d' apis/namespaced/zz_register.go
     sed -i '12d;20d;34d' internal/controller/cluster/zz_setup.go
     sed -i '12d;20d;34d' internal/controller/namespaced/zz_setup.go
+
+    make generate
   '';
 }
