235954619af129d8fa844cb6510ee97062534796
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-07 14:07:37 -0500

    Consolidate packages/*.nix

--- packages/default.nix
+++ packages/default.nix
@@ -2,29 +2,74 @@
   imports = [
     ./apis
     ./kube-vip
-    ./dotnet.nix
-    ./go.nix
     ./images
-    ./upjet.nix
   ];
 
   perSystem =
-    { pkgs, lib, ... }:
+    {
+      inputs',
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
     let
       callPackage = lib.callPackageWith (packages // pkgs);
 
       packages = {
+        inherit (self'.legacyPackages) mangoTools upjetTools;
+        inherit (inputs'.gomod2nix.legacyPackages) buildGoApplication;
+
+        aspire-cli = callPackage ./aspire-cli { };
         awxkit = callPackage ./awxkit { };
+        chart-releaser = callPackage ./chart-releaser { };
+        kubectl-get-all = callPackage ./kubectl-get-all { };
+        kubectl-get-resources = callPackage ./kubectl-get-resources { };
+        kubectl-slice = callPackage ./kubectl-slice { };
+        mmake = callPackage ./mmake { };
         omnissa-horizon-client = callPackage ./omnissa-horizon-client { };
+        openshift-installer = callPackage ./openshift-installer { };
+        smarter-device-manager = callPackage ./smarter-device-manager { };
+        terraform-plugin-codegen-openapi = callPackage ./terraform-plugin-codegen-openapi { };
+        terraform-provider-pfsense = callPackage ./terraform-provider-pfsense { };
+        terraform-providers = callPackage ./terraform-providers { };
+        upjet-provider-cloudflare = callPackage ./upjet-provider-cloudflare { };
       };
     in
     {
       packages = {
-        inherit (packages) awxkit omnissa-horizon-client;
+        inherit (packages)
+          aspire-cli
+          awxkit
+          chart-releaser
+          kubectl-get-all
+          kubectl-get-resources
+          kubectl-slice
+          mmake
+          omnissa-horizon-client
+          openshift-installer
+          smarter-device-manager
+          terraform-plugin-codegen-openapi
+          terraform-provider-pfsense
+          upjet-provider-cloudflare
+          ;
       };
 
       legacyPackages = {
-        inherit (packages) awxkit omnissa-horizon-client;
+        inherit (packages)
+          aspire-cli
+          awxkit
+          chart-releaser
+          kubectl-get-all
+          kubectl-get-resources
+          mmake
+          omnissa-horizon-client
+          openshift-installer
+          smarter-device-manager
+          terraform-plugin-codegen-openapi
+          terraform-providers
+          upjet-provider-cloudflare
+          ;
       };
     };
 }
