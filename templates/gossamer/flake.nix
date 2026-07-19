9188ab778961287eef7a2482b772fcbf5b84402a
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-07-19 17:08:45 -0500

    gossamer template

---
+++ templates/gossamer/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A Gossamer flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    mangopkgs = {
+      url = "github:unmango/pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, inputs', ... }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = [
+              inputs'.mangopkgs.packages.gossamer
+              pkgs.gnumake
+              pkgs.nixfmt
+            ];
+          };
+
+          treefmt.programs = {
+            nixfmt.enable = true;
+          };
+        };
+    };
+}
